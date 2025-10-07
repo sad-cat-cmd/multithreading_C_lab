49d456ff956e5b1a84553aebbaa4497af92611d0
123 <vadim.polinevskiy06@mail.ru>
2025-10-07 15:07:36 +0300

    added lab_2 optimized code and edited report

--- App_2/Project1/Source.c
+++ App_2/Project1/Source.c
@@ -1,21 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <Windows.h>
 
 void print_word(char**);
-size_t get_words_count(char*);
 DWORD WINAPI word_thread(PVOID);
 
-
-CRITICAL_SECTION _crit_section;
-char _output_strign[] = "The AMD641 architecture2 is an extension of the x86 architecture. Any processor implementing the AMD64 architecture specification will also provide compatibility modes for previous descendants of the Intel 8086 architecture, including 32 - bitprocessors such as the Intel 386, Intel Pentium, and AMD K6 - 2 processor. ";
-char* _ptr_string = _output_strign;
-size_t _size_string;
-size_t _current_word = 0;	
+CRITICAL_SECTION crit_section;
+char output_strign[] = "The AMD641 architecture2 is an extension of the x86 architecture. Any processor implementing the AMD64 architecture specification will also provide compatibility modes for previous descendants of the Intel 8086 architecture, including 32 - bitprocessors such as the Intel 386, Intel Pentium, and AMD K6 - 2 processor. ";
+char* ptr_string = output_strign;
 
 int main() {
-	InitializeCriticalSection(&_crit_section);
-	_size_string = get_words_count(_ptr_string);
-	printf("COUNT WORDS: %zu \n\n", _size_string);
+	InitializeCriticalSection(&crit_section);
 	HANDLE _thread_1 = CreateThread(NULL,
 									0,
 									word_thread,
@@ -32,7 +28,7 @@ int main() {
 									word_thread,
 									(LPVOID)2,
 									0,
-									NULL);;
+									NULL);
 	if (_thread_2 == NULL) {
 		printf("Thread 2 has not been created");
 		return 0;
@@ -43,38 +39,32 @@ int main() {
 
 	CloseHandle(_thread_1);
 	CloseHandle(_thread_2);
-	DeleteCriticalSection(&_crit_section);
-	_ptr_string = _output_strign;
+	DeleteCriticalSection(&crit_section);
 	return 0;
 }
 
-size_t get_words_count(char* _ptr) {
-	size_t _count_words = 0;
-	while (*_ptr != '\0') {
-		if (*_ptr == ' ') _count_words++;
-		_ptr++;
-	}
-	_ptr_string = _output_strign;
-	return _count_words + 1;
-}
-
 void print_word(char** ptr_ptr) {
+	char temp[20];
+	size_t index = 0;
+	char* prev_ptr = *ptr_ptr;
 	while (**ptr_ptr != '\0' && **ptr_ptr != ' ') {
-		printf("%c" , **ptr_ptr);
 		(*ptr_ptr)++;
+		index++;
 	}
-	printf("\n");
+	const int i = index;
+	temp[i] = '\0';
+	strncpy(temp, prev_ptr, index);
+	printf("%s\n",temp);
 	(*ptr_ptr)++;
 }
 
 DWORD WINAPI word_thread(PVOID pv_Param) {
 	DWORD _current_thread_id = GetCurrentThreadId();
-	while (_current_word < _size_string) {
-		EnterCriticalSection(&_crit_section);
+	while (*ptr_string != '\0') {
+		EnterCriticalSection(&crit_section);
 		printf("Thread %d is worked (ID: %lu)\n", (int)(INT_PTR)pv_Param, _current_thread_id);
-		print_word(&_ptr_string);
-		_current_word++;
-		LeaveCriticalSection(&_crit_section);
+		print_word(&ptr_string);
+		LeaveCriticalSection(&crit_section);
 		Sleep(10);
 	}
 	return 0; 
