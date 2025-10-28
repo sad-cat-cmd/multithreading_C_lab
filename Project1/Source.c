3613765ee83c84c938ac443efce769b0e8e691e8
123 <vadim.polinevskiy06@mail.ru>
2025-10-28 09:49:38 +0300

    1 fix

--- Project1/Source.c
+++ Project1/Source.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <Windows.h>
+#include <string.h>
+#include <conio.h>
+#include <sysinfoapi.h>
+
+void ConvSysTimeToFileTime(SYSTEMTIME *server_st,SYSTEMTIME *client_st,
+						   FILETIME *server_ft, FILETIME *client_ft) {
+	return;
+}
 
 int main() {
-
+	SYSTEMTIME server_st, client_st;
+	GetSystemTime(&server_st);
+	HANDLE hMailSlot;
+	char chBuffer[256], tmp[256];
+	DWORD dwBytesRead;
+	GetSystemTime(&server_st);
+	
+	/*printf("%d.%d.%d -- %d.%d", server_sys_time.wYear,
+					   server_sys_time.wMonth,
+		               server_sys_time.wDay,
+					   server_sys_time.wHour,
+					   server_sys_time.wSecond);*/
 	return 1;
 }
